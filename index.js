80fc164014fccee67e229dd2ede7c0e4a01edad3
Rachel Hamby <tnthreat33@gmail.com>
2022-12-19 11:58:42 -0500

    commit tree 87c48eba2fb889868ee0054079acd04c02ad39c3

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //isPalidrome needs to take a string and determine if the string is the same forward and backwards if it is then it returns true and if it isn't then it returns false 
+let wordBackwards = "";
+for(let i=word.length-1; i>=0; i--) {
+  wordBackwards = wordBackwards + word.charAt(i);
+}
+if (word === wordBackwards){
+  return true
+}else {
+  return false
+}
 }
 
 /* 
-  Add your pseudocode here
+  function isPalindrome receives one word string. 
+  create a new varible wordBackwards that is assigned the string backwards.
+  to get the wordBackwards iterate through each letter and adding it to word using charAt.
+  if word is equal to wordBackward then return true
+  Else return false
 */
 
 /*
-  Add written explanation of your solution here
+  I had the function take the string and reverse it before then looking to see if the original word matches the new word. 
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("exepecting: false");
+  console.log("=>", isPalindrome("rachel"))
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
